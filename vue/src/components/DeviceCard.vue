20b1610343e592ddd9afb657721427a334f37b72
Bailey Baker <baileybaker919@gmail.com>
2020-08-07 11:30:10 -0400

    CSS and HTML for in use

--- vue/src/components/DeviceCard.vue
+++ vue/src/components/DeviceCard.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="card" v-bind:class="{ 'batteryLow' : (device.batteryLow)  }">
-        <h3> Device Name: {{device.name}} </h3> 
+  <div class="card" v-bind:class="{ 'batteryLow' : (device.batteryLow), 'sensor' : (device.machineModelId == 2) }">
+        <h3  v-if="device.inUse"><div class="circle"></div></h3> 
+        <h3> Device Name: {{device.name}} </h3>
         <h3> Serial: {{device.serial}} </h3>
         <h3> Battery: {{device.batteryLevel}}% </h3> 
         <h3> Device Type: {{device.machineModelId}} </h3>
+        <h3 v-if="device.inUse">Currently In Use</h3>
+        <h3 v-else>Not In Use</h3>
   </div>
 </template>
 
@@ -49,7 +52,7 @@ export default {
 <style>
 .card {
     border: 2px solid black;
-    border-radius: 10px;
+    border-radius: 50px;
     margin: 20px;
     width: 500px;
     text-align: center;
@@ -57,5 +60,16 @@ export default {
 }
 .batteryLow {
     background-color: rgb(250, 66, 66);
+}
+.sensor {
+    border-radius: 0px;
+}
+.circle {
+    height: 20px;
+    width: 20px;
+    border-radius: 10px;
+    background-color: green;
+    margin-top: 8px;
+    margin-left: 450px;
 }
 </style>
