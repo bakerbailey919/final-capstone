cf152225e8407d6e1a01d73d0ef2b57b204fc0a6
Bailey Baker <baileybaker919@gmail.com>
2020-08-11 11:08:27 -0400

    refresh button working

--- vue/src/components/DeviceCard.vue
+++ vue/src/components/DeviceCard.vue
@@ -1,96 +1,97 @@
 <template>
-  <div class="card" v-bind:class="{ 'batteryLow' : (device.batteryLow), 'sensor' : (device.machineModelId == 2), 'connectionLost' : (device.connectionLost) }">
-        <h3  v-if="device.inUse">
-            <div class="circle"></div>
-        </h3> 
-        <h3 v-else>
-            <div class="grayCircle"></div>
-        </h3>
-        <h3> Device Name: {{device.name}} </h3>
-        <h3> Serial: {{device.serial}} </h3>
-        <h3> Battery: {{device.batteryLevel}}% </h3> 
-        <h3> Device Type: {{device.machineModelId}} </h3>
-        <h3 v-if="device.inUse">Currently In Use</h3>
-        <h3 v-else>Not In Use</h3>
-        <h3 v-if="device.batteryLow">Battery is Low</h3>
-        <h3 v-else>Battery Good</h3>
+  <div
+    class="card"
+    v-bind:class="{ 'batteryLow' : (device.batteryLow), 'sensor' : (device.machineModelId == 2), 'connectionLost' : (device.connectionLost) }"
+  >
+    <h3 v-if="device.inUse">
+      <div class="circle"></div>
+    </h3>
+    <h3 v-else>
+      <div class="grayCircle"></div>
+    </h3>
+    <h3>Device Name: {{device.name}}</h3>
+    <h3>Serial: {{device.serial}}</h3>
+    <h3>Battery: {{device.batteryLevel}}%</h3>
+    <h3>Device Type: {{device.machineModelId}}</h3>
+    <h3 v-if="device.inUse">Currently In Use</h3>
+    <h3 v-else>Not In Use</h3>
+    <h3 v-if="device.batteryLow">Battery is Low</h3>
+    <h3 v-else>Battery Good</h3>
 
-        <p> Left Distance Since Maintenance: {{device.leftDistanceSinceMaintenance}} </p>
-        <p> Right Distance Since Maintenance: {{device.rightDistanceSinceMaintenance}} </p>
+    <p>Left Distance Since Maintenance: {{device.leftDistanceSinceMaintenance}}</p>
+    <p>Right Distance Since Maintenance: {{device.rightDistanceSinceMaintenance}}</p>
 
-        <button class="maintenanceButton" v-on:click="sendSerial()"> Maintenance Reset </button>
+    <button class="maintenanceButton" v-on:click="sendSerial()">Maintenance Reset</button>
   </div>
 </template>
 
 
 <script>
-
-import DeviceService from '../services/DeviceService.js'
+import DeviceService from "../services/DeviceService.js";
 
 export default {
-    name: 'device-card',
-    props: {
-        device: Object
-    },
-    data() {
-    return {
-      
-
-    };
+  name: "device-card",
+  props: {
+    device: Object,
   },
-  created() {
+  data() {
+    return {};
   },
+  created() {},
   methods: {
-      sendSerial() {
-          DeviceService.sendSerial(this.device.serial)
-          .then ( () => {
-               console.log("Maintenace Checkpoint Updated") 
-          })
-          .catch( (error) => {
+    sendSerial() {
+      DeviceService.sendSerial(this.device.serial)
+        .then(() => {
+          console.log("Maintenace Checkpoint Updated");
+          window.location.reload();
+        })
+        .catch((error) => {
           console.error(error);
-      });
-      }
-  }
-}
+        });
+    },
+    reloadPage() {
+      window.location.reload();
+    },
+  },
+};
 </script>
 
 <style>
 .card {
-    border: 2px solid black;
-    border-radius: 50px;
-    margin: 20px;
-    width: 500px;
-    text-align: center;
-    background-color: lightgreen;
-    
+  border: 2px solid black;
+  border-radius: 50px;
+  margin: 20px;
+  width: 500px;
+  text-align: center;
+  background-color: lightgreen;
 }
 .batteryLow {
-    background-color:lightsalmon;
+  background-color: lightsalmon;
 }
 .connectionLost {
-    background-color: rgb(250, 66, 66);
+  background-color: rgb(250, 66, 66);
 }
 .sensor {
-    border-radius: 0px;
+  border-radius: 0px;
 }
 .circle {
-    height: 20px;
-    width: 20px;
-    border-radius: 10px;
-    background-color:green;
-    margin-top: 8px;
-    margin-left: 450px;
+  height: 20px;
+  width: 20px;
+  border-radius: 10px;
+  background-color: green;
+  margin-top: 8px;
+  margin-left: 450px;
 }
 .grayCircle {
-    height: 20px;
-    width: 20px;
-    border-radius: 10px;
-    background-color: darkgray;
-    margin-top: 8px;
-    margin-left: 450px;
+  height: 20px;
+  width: 20px;
+  border-radius: 10px;
+  background-color: darkgray;
+  margin-top: 8px;
+  margin-left: 450px;
 }
 .maintenanceButton {
-    width: 150px;
-    margin-left: 175px;
+  width: 150px;
+  margin-left: 175px;
 }
 </style>
