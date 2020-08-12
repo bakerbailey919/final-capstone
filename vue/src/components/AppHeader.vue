96aa1a1bd40ca7143b058985e7a778832aeb17f5
Bailey Baker <baileybaker919@gmail.com>
2020-08-12 13:29:26 -0400

    navbar styling

--- vue/src/components/AppHeader.vue
+++ vue/src/components/AppHeader.vue
@@ -1,24 +1,27 @@
 <template>
   <header>
     <nav class="navbar navbar-light fixed-top">
-      <span class="navbar-brand mb-0 h1">
-        <a class="navbar-brand" href="#">
-          <router-link v-bind:to="{name: 'home'}">
-            <img
-              class="logo-img, d-inline-block align-top"
-              src="../assets/images/Include_Health_logo_with_company_name.png"
-              width="600"
-              height="100"
-              alt="Logo"
-            />
-          </router-link>
-        </a>
-        <router-link v-bind:to="{name: 'devices'}" exact>Devices</router-link>
-        <router-link v-bind:to="{name: 'machineCheckins'}" exact>&nbsp;|&nbsp;Check-In Log</router-link>
-        <router-link v-bind:to="{name: 'register'}" v-if="!$store.state.token">&nbsp;|&nbsp;Register</router-link>
-        <router-link v-bind:to="{name: 'login'}" v-if="!$store.state.token">&nbsp;|&nbsp;Login</router-link>
-        <router-link v-bind:to="{name: 'logout'}" v-if="$store.state.token">&nbsp;|&nbsp;Logout</router-link>
-      </span>
+      <!-- <span class="navbar-brand mb-0 h1"> -->
+        <router-link v-bind:to="{name: 'home'}" class="navbar-brand" href="#">
+          <img
+            class="logo-img, d-inline-block align-top"
+            src="../assets/images/Include_Health_logo_with_company_name.png"
+            width="300"
+            height="50"
+            alt="Logo"
+          />
+        </router-link>
+        <div class="nav-buttons">
+          <router-link v-bind:to="{name: 'home'}" exact>Devices</router-link>
+          <router-link v-bind:to="{name: 'machineCheckins'}" exact>&nbsp;|&nbsp;Check-In Log</router-link>
+          <router-link
+            v-bind:to="{name: 'register'}"
+            v-if="!$store.state.token"
+          >&nbsp;|&nbsp;Register</router-link>
+          <router-link v-bind:to="{name: 'login'}" v-if="!$store.state.token">&nbsp;|&nbsp;Login</router-link>
+          <router-link v-bind:to="{name: 'logout'}" v-if="$store.state.token">&nbsp;|&nbsp;Logout</router-link>
+        </div>
+      <!-- </span> -->
     </nav>
   </header>
 </template>
@@ -34,7 +37,7 @@ export default {
 
 <style scoped>
 header {
-  height: 146px;
+  height: 86px;
   color: #81888b;
   grid-area: navBar;
   text-align: center;
@@ -42,5 +45,11 @@ header {
 nav {
   border-bottom: 10px solid #2dacb7;
   background-color: #f3f3f3;
+}
+.nav-buttons {
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: row;
+  align-items: center;
 }
 </style>
